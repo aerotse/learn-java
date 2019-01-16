249c6ed5d84d28c1784f48e6e13e0040ab5d4162
xieth <xieth@317hu.com>
2019-01-16 11:02:45 +0800

    test2

--- src/main/java/org/wcong/test/autovalue/TestNew.java
+++ src/main/java/org/wcong/test/autovalue/TestNew.java
@@ -4,4 +4,7 @@ package org.wcong.test.autovalue;
  * Created by xieth on 2019/1/16.
  */
 public class TestNew {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 }
